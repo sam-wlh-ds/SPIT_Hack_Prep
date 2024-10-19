3d7c667410eef8fa6abf37e2855ac98a5e961ff8
kalp_1210 <rambhiakalp@gmail.com>
2024-10-19 14:54:51 +0530

    first push

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.css'
-import { RouterProvider } from 'react-router-dom';
-import router from './routes';
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
+
+// hi kalp here
