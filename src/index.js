b3a91c1a311661cc3f9272df309f177621007da8
sam_wlh_ds <sam.wlh.ds@gmail.com>
2024-10-19 20:54:38 +0530

    Added Carousel Card

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
